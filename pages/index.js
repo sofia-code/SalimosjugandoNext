7bcdba8ef43f3bd89a4252f5c769c43ed3f0f60d
sofia-code <sofihadjilias@gmail.com>
2020-09-19 20:34:22 -0300

    Change route again lpm

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import auth0 from '../lib/auth0'
 import {Layout, Home as HomeContainer} from "../containers";
 
-function Home(){
+function Index(){
     return(
         <Layout>
             <HomeContainer />
@@ -10,18 +10,17 @@ function Home(){
     )
 }
 
-export async function getServerSideProps({ req, res }) {
-    const session = await auth0.getSession(req);
-    if (!session || !session.user) {
-      res.writeHead(302, {
-        Location: "/api/login",
-      });
-      res.end();
-      return {};
-    }
-    return { props: { user: session.user } };
-  }
+// export async function getServerSideProps({ req, res }) {
+//     const session = await auth0.getSession(req);
+//     if (!session || !session.user) {
+//       res.writeHead(302, {
+//         Location: "/api/login",
+//       });
+//       res.end();
+//       return {};
+//     }
+//     return { props: { user: session.user } };
+//   }
 
 
-
-export default Home;
+export default Index;
